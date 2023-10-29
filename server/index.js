4e7c2d0625c642293d47a34db1aedcea176f0860
akhilesh <akhileshkumar@gofynd.com>
2023-10-30 00:30:44 +0530

    h

--- server/index.js
+++ server/index.js
@@ -223,12 +223,12 @@ app.post("/reset-password/:id/:token",(req, res)=> {
 })
 if (process.env.NODE_ENV === 'production') {
   //app.use(express.static(__dirname + '/../public/'))
-  app.use(express.static(__dirname + './build'))
+  app.use(express.static(__dirname + '/../build'))
 
 
 
   app.get('/.*/', (req, res) => {
-      res.sendFile(__dirname + './build/index.html');
+      res.sendFile(__dirname + '/../build/index.html');
   });
 }
 
