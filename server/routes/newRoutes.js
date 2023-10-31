2dbba48b3d73fc19904320f441bb40accfab28e4
akhilesh <akhileshkumar@gofynd.com>
2023-10-31 16:56:24 +0530

    u

--- server/routes/newRoutes.js
+++ server/routes/newRoutes.js
@@ -20,38 +20,38 @@ const AuthenticateUser = (req, res, next) => {
         res.status(401).json({ message: 'unauthorized hi' })
     }
 
-    // jwt.verify(token, jwtSecret, { expiresIn: '24hr' }, (err, user) => {
-    //     console.log(token)
-    //     console.log(user)
-    //     if (err) {
-    //         console.log(err);
-    //         return res.status(401).json({ message: 'Failed to authenticate token' });
+    jwt.verify(token, jwtSecret, { expiresIn: '24hr' }, (err, user) => {
+        console.log(token)
+        console.log(user)
+        if (err) {
+            console.log(err);
+            return res.status(401).json({ message: 'Failed to authenticate token' });
 
-    //     }
-    //     req.user = user;
-
-
-    //     return next();
-
-
-
-    // });
-    try {
-        const decoded = jwt.verify(token, jwtSecret);
-
-        if (decoded) {
-
-          if (decoded.exp - Date.now() / 1000 < 1800) {
-
-            const newToken = jwt.sign({ user: decoded.user }, jwtSecret, { expiresIn: '1d' });
-            res.cookie('token', newToken, { maxAge: 24 * 60 * 60 * 1000 });
-          }
         }
-      } catch (err) {
-          return res.status(500).send({message:"hui"})
-      }
+        req.user = user;
 
-      next();
+
+        return next();
+
+
+
+    });
+//     try {
+//         const decoded = jwt.verify(token, jwtSecret);
+
+//         if (decoded) {
+
+//           if (decoded.exp - Date.now() / 1000 < 1800) {
+
+//             const newToken = jwt.sign({ user: decoded.user }, jwtSecret, { expiresIn: '1d' });
+//             res.cookie('token', newToken, { maxAge: 24 * 60 * 60 * 1000 });
+//           }
+//         }
+//       } catch (err) {
+//           return res.status(500).send({message:"hui"})
+//       }
+
+//       next();
 
 }
 
