54306228879c3a120d8c2038aa836fedc08601fd
akhilesh <akhileshkumar@gofynd.com>
2023-11-07 22:01:26 +0530

    1261

--- server/index.js
+++ server/index.js
@@ -129,13 +129,13 @@ app.get('/chemicals',AuthenticateUser, async (req, res) => {
   }
 });
 
-app.post('/chemicals/remove/:name', async (req, res) => {
+app.post('/chemicals/remove/:name',AuthenticateUser, async (req, res) => {
   const chemicalName = req.params.name;
   const { quantityRemoved } = req.body;
+  const chemicalId = chemicalName._id;
 
 
-
-  const y = await ChemicalsModel.findOne({ chemicalName: chemicalName })
+  const y = await ChemicalsModel.findOne({ chemicalId: chemicalId })
     .then((chemicals) => {
       if (!chemicals) {
         return res.status(400).json({ message: "not found" });
